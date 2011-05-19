5c5882993dda2c62cdd2b8e9b3a43a2b445af5cd
Joachim Schuster <joachim.schuster@catchingtales.de>
2011-05-19 17:20:10 +0200

    Fixed missing leading zero for single digit hex values.
    Closes #39

--- GreenDroid/src/greendroid/util/Md5Util.java
+++ GreenDroid/src/greendroid/util/Md5Util.java
@@ -4,35 +4,39 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Md5Util {
-    
+
     private static MessageDigest sMd5MessageDigest;
     private static StringBuilder sStringBuilder;
-    
+
     static {
         try {
             sMd5MessageDigest = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
-            // TODO cyril: I'm quite sure about my "MD5" algorithm
+            // TODO Cyril: I'm quite sure about my "MD5" algorithm
             // but this is not a correct way to handle an exception ...
         }
         sStringBuilder = new StringBuilder();
     }
-    
+
     private Md5Util() {
     }
-    
+
     public static String md5(String s) {
-        
+
         sMd5MessageDigest.reset();
         sMd5MessageDigest.update(s.getBytes());
-        
+
         byte digest[] = sMd5MessageDigest.digest();
-        
+
         sStringBuilder.setLength(0);
-        for (int i=0; i<digest.length; i++) {
-            sStringBuilder.append(Integer.toHexString(0xFF & digest[i]));
+        for (int i = 0; i < digest.length; i++) {
+            final int b = digest[i] & 255;
+            if (b < 16) {
+                sStringBuilder.append('0');
+            }
+            sStringBuilder.append(Integer.toHexString(b));
         }
-                
+
         return sStringBuilder.toString();
     }
 }
